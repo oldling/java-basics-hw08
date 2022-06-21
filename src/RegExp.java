365bb84bec16ca5ff350d13b12befde410af6613
oldling <oldling@gmail.com>
2022-06-21 14:25:07 +0300

    getIpAddress done

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
@@ -9,8 +12,19 @@ public class RegExp {
      * @param text text to parse
      * @return String with IP address
      */
-    public static String getIpAddress(String text) {
-        // TODO fill in code here for extracting IP address using Regular Expression
-        return "";
+    public static String getIpAddress(String text){
+        String num = "25[0-5]|2[0-4][0-9]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9]";
+        String valid = String.format("((%s)(\\.)){3}(%s)", num, num);
+        String result = "";
+
+        Pattern pattern = Pattern.compile(valid);
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            result = matcher.group();
+        }
+        return result;
     }
 }
+
+
--- src/RegExpTest.java
+++ src/RegExpTest.java
@@ -13,16 +13,18 @@ public class RegExpTest {
     @DataProvider(name = "text")
     public static Object[][] text() {
         return new Object[][]{
-                {"8.8.8.8", "8.8.8.8"},
-                {"http://192.168.1.1/index.html", "192.168.1.1"},
-                {"This text contains a valid IP address 254.254.254.254, find it!", "254.254.254.254"},
-                {"IP could not have negative numbers like -1.-2.-3.-4", ""},
-                {" Invalid IP: 300.300.300.300!", ""},
-                {" Invalid IP: 256.256.256.256!", ""},
-                {"invalid:256.256.256.256", ""},
-                {"valid IP: 255.255.255.255!", "255.255.255.255"},
-                {"invalid:255.255.255.255", "255.255.255.255"}
-                // TODO add 2 more test data here
+            {"8.8.8.8", "8.8.8.8"},
+            {"http://192.168.1.1/index.html", "192.168.1.1"},
+            {"This text contains a valid IP address 254.254.254.254, find it!", "254.254.254.254"},
+            {"IP could not have negative numbers like -1.-2.-3.-4", ""},
+            {" Invalid IP: 300.300.300.300!", ""},
+            {" Invalid IP: 256.256.256.256!", ""},
+            {"invalid:256.256.256.256", ""},
+            {"valid IP: 255.255.255.255!", "255.255.255.255"},
+            {"invalid:255.255.255.255", "255.255.255.255"},
+            {"The address 0.0.0.0 is a non-routable meta-address used to designate an invalid, unknown or non-applicable target", "0.0.0.0"},
+            {"IP: 255.255.255,255", ""},
+            {"IP: 255.255 255.255", ""}
         };
     }
 
